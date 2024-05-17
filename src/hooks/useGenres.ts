841671baf24f1529960eda07073317619130ec6b
oswaldo vega <osvegal90@gmail.com>
2024-05-17 08:34:19 -0500

    refactor time calculations

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient, { FetchResponse } from "../services/api-client";
 import genres from "../data/genres";
+import ms from "ms";
 
 const apiClient = new APIClient<Genre>("/genres");
 
@@ -14,7 +15,7 @@ const useGenres = () =>
   useQuery<FetchResponse<Genre>>({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    staleTime: ms("1d"),
     initialData: genres,
   });
 
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient, { FetchResponse } from "../services/api-client";
 import platforms from "../data/platforms";
+import ms from "ms";
 
 const apiClient = new APIClient<Platform>("/platforms/lists/parents");
 
@@ -14,7 +15,7 @@ const usePlatforms = () =>
   useQuery<FetchResponse<Platform>>({
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
-    staleTime: 5 * 60 * 1000, // 5mins
+    staleTime: ms("5m"),
     initialData: platforms,
   });
 
